9c916a23f9c866848b80c9dd8bcc38b189e71b53
atkthefixyou <nguyenngoctrai982@gmail.com>
2026-06-23 05:40:55 +0700

    fix: clean BlobBuddyMod.java

--- src/main/java/com/blobbuddy/BlobBuddyMod.java
+++ src/main/java/com/blobbuddy/BlobBuddyMod.java
@@ -10,48 +10,6 @@ import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.level.Level;
-
-public class BlobBuddyMod implements ModInitializer {
-    public static final String MOD_ID = "blob-buddy";
-    public static EntityType<BlobEntity> BLOB_ENTITY;
-
-    @Override
-    public void onInitialize() {
-        var key = net.minecraft.resources.ResourceKey.create(
-            BuiltInRegistries.ENTITY_TYPE.key(),
-            Identifier.of(MOD_ID, "blob")
-        );
-        BLOB_ENTITY = Registry.register(
-            BuiltInRegistries.ENTITY_TYPE,
-            key,
-            FabricEntityTypeBuilder.create(MobCategory.MISC, BlobEntity::new)
-                .dimensions(EntityDimensions.scalable(0.8f, 0.8f))
-                .build(key)
-        );
-        BlobPackets.registerServerPackets();
-    }
-}
-EO
-
-
-
-
-
-
-cat > src/main/java/com/blobbuddy/BlobBuddyMod.java << 'EOF'
-package com.blobbuddy;
-
-import com.blobbuddy.entity.BlobEntity;
-import com.blobbuddy.network.BlobPackets;
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.Identifier;
-import net.minecraft.world.entity.EntityDimensions;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobCategory;
 
 public class BlobBuddyMod implements ModInitializer {
     public static final String MOD_ID = "blob-buddy";
